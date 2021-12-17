865ed99fbb53963119b28b9954b80139d7a76007
PawelPMaziarz <w64155@student.wsiz.edu.pl>
2021-12-17 11:11:00 +0100

    Added database copy and sample api key

--- myWallet/Secrets.cs
+++ myWallet/Secrets.cs
@@ -2,7 +2,7 @@
 {
     public partial class Secrets
     {
-        public static string apiKey = "";
+        public static string apiKey = "6SUJGZAN9TN05M78"; //example apikey with limitations
         public static string dbUser = "";
         public static string dbPassword = "";
         public static string dbName = "";
